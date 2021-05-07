29689c70f5cc406e184bcf7d357b45a78598ff01
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-05-08 00:30:26 +0600

    bug fix

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ cmd.addEventListener("keypress", function (e) {
   details.classList.remove("details_anim");
   var cmd_prmpt = cmd.value;
   cmd_prmpt = cmd_prmpt.toLowerCase();
-  var error = "<span class='error'>" + cmd_prmpt + " not found, try command: info, skills, projects or clear</span>";
+  var error = "<span class='error'>" + cmd_prmpt + " not found, try command: info, about, skills, projects, education, achievements or clear</span>";
 
   if (e.key === 'Enter') {
     details.classList.add("details_anim");
